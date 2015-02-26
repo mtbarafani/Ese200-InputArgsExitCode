8ab05d3762e2a13bfba044427699bb1c6eb04e85
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-02-26 16:49:57 +0100

    Completata

--- ese200.sh
+++ ese200.sh
@@ -3,13 +3,31 @@
 # Il mio primo script per shell Linux/Unix
 
 echo "Il nome del file eseguibile e' \"$0\""
-echo "L'argomento 1 e': \"$1\""
-echo "L'argomento 2 e': \"$2\""
+echo
 
-exit 24
+if [ $# -gt 1 ]
+then
+	i=0
+	for arg in "$@"
+	do
+		i=$(($i + 1))
+	    echo "L'argomento $i e': \"$arg\""
+	done
+else
+	echo 'Hai invocato il programma senza alcun argomento.'
+    echo 'Riprova, specificando gli argomenti dopo il nome del programma, separandoli con uno spazio.'
+fi
 
-# Amplia lo script
+r=$(($RANDOM % 256))
+echo
+echo "Ho generato il numero casuale $r: lo utilizzo come exit code."
+echo 'Per visualizzarlo, digita (senza virgolette)'
+echo '- in Prompt dei Comandi: "echo %ERRORLVEL%"'
+echo '- in Linux (o CygWin):   "echo $?"'
+exit $r
+
+# Osservazioni
 #
-# 1) Come ottengo il numero degli argomenti di input specificati?
-# 2) Come posso fare un ciclo for che stampi tutti gli argomenti specificati?
-# 3) Come posso generare un codice di uscita casuale?
+# 1) Nota che alcune stringhe hanno virgolette doppie, mentre altre hanno virgolette singole: qual è la differenza?
+# 2) Nota che richiamare echo senza argomento stampa una riga vuota
+# 3) Nota la sintassi del for (una delle possibili): prova a fare variazioni

a9e5fa881ceffca76cfd149202d3052c90d7660e
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-02-26 16:38:59 +0100

    Inizio

---
+++ ese200.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Il mio primo script per shell Linux/Unix
+
+echo "Il nome del file eseguibile e' \"$0\""
+echo "L'argomento 1 e': \"$1\""
+echo "L'argomento 2 e': \"$2\""
+
+exit 24
+
+# Amplia lo script
+#
+# 1) Come ottengo il numero degli argomenti di input specificati?
+# 2) Come posso fare un ciclo for che stampi tutti gli argomenti specificati?
+# 3) Come posso generare un codice di uscita casuale?
